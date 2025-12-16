59955d2eeee6e83871b36b2df5b2342d0336f34a
wcrvt <cat0127.k@gmail.com>
2025-12-16 20:01:03 +0900

    add multi-plcs support

--- src/commands/read/mod.rs
+++
@@ -1,8 +0,0 @@
-mod bulk;
-pub use bulk::{SLMPBulkReadCommand, SLMPBulkReadQuery};
-
-mod random;
-pub use random::{SLMPRandomReadCommand, SLMPRandomReadQuery};
-
-mod block;
-pub use block::{SLMPBlockReadCommand, SLMPBlockReadQuery};
--- src/commands/write/mod.rs
+++
@@ -1,9 +0,0 @@
-
-mod bulk;
-pub use bulk::{SLMPBulkWriteCommand, SLMPBulkWriteQuery};
-
-mod random;
-pub use random::{SLMPRandomWriteCommand, SLMPRandomWriteQuery};
-
-mod block;
-pub use block::{SLMPBlockWriteCommand, SLMPBlockWriteQuery};
